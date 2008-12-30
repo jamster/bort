5adefdd2953940c9b895c75cabe5395794e58d8e
Jason Amster <jayamster@gmail.com>
2008-12-30 00:51:19 -0500

    Needed to run it via 'run'

--- config/deploy.rb
+++ config/deploy.rb
@@ -37,7 +37,7 @@ namespace :db do
   
   desc "Create the database"
   task :create do 
-    `cd #{current_release} && rake db:create RAILS_ENV=#{rails_env} --trace`
+    run "cd #{current_release} && rake db:create RAILS_ENV=#{rails_env} --trace"
   end
 
 end
